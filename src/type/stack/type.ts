00174cd0cccf4c341dc43fa195d294ea8588e682
JJHyeok13 <jjinh99@naver.com>
2025-01-07 09:27:18 +0900

    feat : 상단 네비게이션 구현을 위한 타입 추가 및 수정

--- src/type/stack/type.ts
+++ src/type/stack/type.ts
@@ -1,22 +1,35 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NativeStackScreenProps, NativeStackNavigationProp } from '@react-navigation/native-stack';
 
+// 로그인 이전 유저에 대한 스크린
 export type RootStackParamList = {
   HomeScreen: undefined;
 };
 
-export type StackParamList = {
-  BottomNavigation: { screen: keyof BottomNavigationParamList } | undefined;
-
-  MainScreen: undefined;
-
-  CreateScreen: undefined;
-};
-
-export type BottomNavigationParamList = {
-  MainScreen: undefined;
-};
-
 export type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'HomeScreen'>;
 
+// 로그인한 유저에 대한 스크린
+export type StackParamList = {
+  MainScreen: undefined;
+  DetailScreen: { id: number };
+
+  // 좋아요 스크린
+  LikeScreen: undefined;
+
+  // 마이페이지 스크린
+  MyPageScreen: undefined;
+};
+
 export type MainScreenProps = NativeStackScreenProps<StackParamList, 'MainScreen'>;
-export type CreateScreenProps = NativeStackScreenProps<StackParamList, 'CreateScreen'>;
+export type PopUpScreenProps = { navigation: MainScreenProps['navigation'] };
+export type RecruitScreenProps = { navigation: MainScreenProps['navigation'] };
+export type DetailScreenProps = NativeStackScreenProps<StackParamList, 'DetailScreen'>;
+export type LikeScreenProps = NativeStackScreenProps<StackParamList, 'LikeScreen'>;
+export type MyPageScreenProps = NativeStackScreenProps<StackParamList, 'MyPageScreen'>;
+
+// 상단 네비게이션
+export type TopNavigationParamList = {
+  LikeScreen: undefined;
+  MyPageScreen: undefined;
+};
+
+export type TopNavigationProp = NativeStackNavigationProp<TopNavigationParamList>;
