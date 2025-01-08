5e4b397e73f91da8de50ae41e4bd2e4ea9c48e73
JJHyeok13 <jjinh99@naver.com>
2025-01-08 20:26:45 +0900

    design : 상단 탭 네비게이션 디자인 수정

--- src/components/topNavigation/topNavigation.tsx
+++ src/components/topNavigation/topNavigation.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { View, Pressable } from 'react-native';
+import React, { useRef, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-
-import CustomText from '@components/common/customText';
+import { View, Text, Animated, Pressable, Dimensions } from 'react-native';
 
 import { TopNavigationProp } from '@type/stack/type';
 
@@ -18,29 +16,55 @@ interface TopNavigationProps {
 const TopNavigation: React.FC<TopNavigationProps> = ({ type, handleType }) => {
   const navigation = useNavigation<TopNavigationProp>();
 
+  const width = Dimensions.get('screen').width;
+  const translateX = useRef(new Animated.Value(0)).current;
+
+  const [switchWidth, setSwitchWidth] = useState<number>(0);
+
+  const handleLayout = (event: any) => {
+    const { width } = event.nativeEvent.layout;
+    setSwitchWidth(width);
+  };
+
+  const slideAnimation = () => {
+    if (type === 'popUp') {
+      handleType('recruit');
+    } else {
+      handleType('popUp');
+    }
+
+    Animated.timing(translateX, {
+      toValue: type === 'recruit' ? 0 : 73,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <View className="fixed z-10 flex h-[72px] w-full flex-row items-center justify-between bg-black px-5 py-3">
+    <View className="fixed z-10 flex h-[72px] w-full flex-row items-center justify-between bg-black py-3 pl-5">
       <Logo />
 
-      <View
-        className={`flex flex-row items-center rounded-full bg-white p-1 ${
+      <Pressable
+        onPress={() => slideAnimation()}
+        onLayout={handleLayout}
+        style={{ left: (width - switchWidth) / 2 }}
+        className={`absolute flex flex-row items-center rounded-full bg-white p-1 ${
           type === 'popUp' ? 'text-white' : 'text-black'
         }`}
       >
-        <Pressable
-          onPress={() => handleType('popUp')}
-          className={`${type === 'popUp' && 'bg-black'} rounded-full p-3`}
-        >
-          <CustomText className={`${type === 'popUp' && 'text-white'}`}>팝업장소</CustomText>
-        </Pressable>
-
-        <Pressable
-          onPress={() => handleType('recruit')}
-          className={`${type === 'recruit' && 'bg-black'} rounded-full p-3`}
-        >
-          <CustomText className={`${type === 'recruit' && 'text-white'}`}>지원공고</CustomText>
-        </Pressable>
-      </View>
+        <Animated.View
+          style={{
+            transform: [{ translateX }],
+          }}
+          className="absolute left-1 top-1 h-full w-1/2 rounded-full bg-black"
+        />
+        <Text className={`${type === 'popUp' && 'text-white'} p-3 font-BTN1 text-BTN1`}>
+          팝업공간
+        </Text>
+        <Text className={`${type === 'recruit' && 'text-white'} p-3 font-BTN1 text-BTN1`}>
+          지원공고
+        </Text>
+      </Pressable>
 
       <View className="flex flex-row">
         <Pressable onPress={() => navigation.navigate('LikeScreen')} className="p-3">
