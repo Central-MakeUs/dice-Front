666a46012bc923d82896545e0b4ec0a3b7521278
JJHyeok13 <jjinh99@naver.com>
2025-01-08 20:48:29 +0900

    chore : 사용해볼수 있도록 하는 예시 지원공고 스크린에 구현

--- src/screens/recruit/recruit.tsx
+++ src/screens/recruit/recruit.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import CustomModal from '@components/common/customModal';
 
 import { RecruitScreenProps } from '@type/stack/type';
 
 const RecruitScreen = ({ navigation }: RecruitScreenProps) => {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
   return (
     <View className="flex-1">
       <Text>지원공고 스크린</Text>
+      <Button title="열기" onPress={() => setIsModalVisible(true)}></Button>
+
+      <CustomModal
+        isVisible={isModalVisible}
+        closeModal={() => setIsModalVisible(false)}
+        title="인증번호가 발송되었습니다"
+        buttonText="확인"
+      />
     </View>
   );
 };
