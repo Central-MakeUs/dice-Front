906648ace5ddd834a8b829f4cb36f978d66bee6c
JJHyeok13 <jjinh99@naver.com>
2025-01-25 03:45:43 +0900

    feat : 칩 컴포넌트 클릭 시 필터링 모달 열리도록 적용

--- src/components/popUp/chipContainer.tsx
+++ src/components/popUp/chipContainer.tsx
@@ -3,14 +3,18 @@ import { View } from 'react-native';
 
 import FilterChip from '@components/common/filterChip';
 
-const ChipContainer: React.FC = () => {
+interface ChipContainerProps {
+  openModal: (text: string) => void;
+}
+
+const ChipContainer: React.FC<ChipContainerProps> = ({ openModal }) => {
   return (
     <View className="flex flex-row justify-between bg-white px-5">
       <View className="flex flex-row py-4">
-        <FilterChip title={'지역'} />
-        <FilterChip title={'가격'} />
-        <FilterChip title={'수용인원'} />
-        <FilterChip title={'인기순'} />
+        <FilterChip title={'지역'} openModal={() => openModal('region')} />
+        <FilterChip title={'가격'} openModal={() => openModal('price')} />
+        <FilterChip title={'수용인원'} openModal={() => openModal('people')} />
+        <FilterChip title={'인기순'} openModal={() => openModal('sort')} />
       </View>
     </View>
   );
