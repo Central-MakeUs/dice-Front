4b6590843077f70946b33c235d672be63fea39f2
JJHyeok13 <jjinh99@naver.com>
2025-02-02 16:07:59 +0900

    fix : 채팅방 컴포넌트 onPress 네비게이션 및 나가기 버튼 모달 구현

--- src/components/chatBox/chatRoom.tsx
+++ src/components/chatBox/chatRoom.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text, View, Pressable } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Text, View, Image } from 'react-native';
+import { Pressable } from 'react-native-gesture-handler';
 import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
 import Reanimated, { SharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
@@ -11,9 +11,14 @@ import { ChatBoxScreenProps } from '@type/stack/type';
 interface ChatRoomComponentProps {
   chatRoomData: ChatRoom;
   navigation: ChatBoxScreenProps['navigation'];
+  handleExitModal: (chatRoomId: number) => void;
 }
 
-const ChatRoomComponent: React.FC<ChatRoomComponentProps> = ({ chatRoomData, navigation }) => {
+const ChatRoomComponent: React.FC<ChatRoomComponentProps> = ({
+  chatRoomData,
+  navigation,
+  handleExitModal,
+}) => {
   function RightAction(prog: SharedValue<number>, drag: SharedValue<number>) {
     const styleAnimation = useAnimatedStyle(() => {
       return {
@@ -24,7 +29,7 @@ const ChatRoomComponent: React.FC<ChatRoomComponentProps> = ({ chatRoomData, nav
     return (
       <Reanimated.View style={styleAnimation}>
         <Pressable
-          onPress={() => console.log('나가기')}
+          onPress={() => handleExitModal(chatRoomData.chatRoomId)}
           className="flex h-full w-[74px] items-center justify-center bg-red"
         >
           <Text className="text-center font-SUB3 text-SUB3 leading-SUB3 text-white">나가기</Text>
@@ -35,17 +40,31 @@ const ChatRoomComponent: React.FC<ChatRoomComponentProps> = ({ chatRoomData, nav
 
   return (
     <ReanimatedSwipeable renderRightActions={RightAction} overshootRight={false}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('LikeScreen')}
+      <Pressable
+        onPress={() => navigation.navigate('ChatRoomScreen', { roomId: chatRoomData.chatRoomId })}
         className="flex flex-row justify-between px-5 py-[15.5px]"
       >
-        <View>
-          <Text className="font-SUB2 text-SUB2 leading-SUB2 text-dark_gray">
-            {chatRoomData.name}
-          </Text>
-          <Text className="font-BODY2 text-BODY2 leading-BODY2 text-deep_gray">
-            {chatRoomData.lastContent}
-          </Text>
+        <View className="flex flex-row space-x-3">
+          <View className="relative">
+            <Image
+              source={{ uri: chatRoomData.storeImage }}
+              className="h-[50px] w-[50px] rounded-lg"
+            />
+
+            <Image
+              source={{ uri: chatRoomData.adminImage }}
+              className="absolute -bottom-2 -right-2 h-8 w-8"
+            />
+          </View>
+
+          <View>
+            <Text className="font-SUB3 text-SUB3 leading-SUB3 text-dark_gray">
+              {chatRoomData.name}
+            </Text>
+            <Text className="font-BODY2 text-BODY2 leading-BODY2 text-medium_gray">
+              {chatRoomData.lastContent}
+            </Text>
+          </View>
         </View>
 
         <View className="flex flex-col items-end justify-between">
@@ -61,7 +80,7 @@ const ChatRoomComponent: React.FC<ChatRoomComponentProps> = ({ chatRoomData, nav
             </View>
           )}
         </View>
-      </TouchableOpacity>
+      </Pressable>
     </ReanimatedSwipeable>
   );
 };
--- src/screens/chatBox/chatBox.tsx
+++ src/screens/chatBox/chatBox.tsx
@@ -5,13 +5,32 @@ import { dummyData } from './dummyData';
 
 import HeaderComponent from '@components/chatBox/header';
 import ChatRoomComponent from '@components/chatBox/chatRoom';
+import CustomTwoBtnModal from '@components/common/customTwoBtnModal';
 
 import SafeArea from '@providers/safeArea';
 
 import { ChatBoxScreenProps } from '@type/stack/type';
 
 const ChatBoxScreen = ({ navigation }: ChatBoxScreenProps) => {
-  const [chatRoomData] = useState(dummyData);
+  const [chatRoomData, setChatRoomData] = useState(dummyData);
+
+  const [isExitModalVisible, setIsExitModalVisible] = useState<boolean>(false);
+  const [selectedChatRoomId, setSelectedChatRoomId] = useState<number | null>(null);
+
+  const handleExitModal = (chatRoomId: number) => {
+    setSelectedChatRoomId(chatRoomId);
+    setIsExitModalVisible(true);
+  };
+
+  const handleExit = () => {
+    if (selectedChatRoomId !== null) {
+      setChatRoomData((prevData) =>
+        prevData.filter((room) => room.chatRoomId !== selectedChatRoomId),
+      );
+      setSelectedChatRoomId(null);
+    }
+    setIsExitModalVisible(false);
+  };
 
   return (
     <SafeArea>
@@ -25,7 +44,11 @@ const ChatBoxScreen = ({ navigation }: ChatBoxScreenProps) => {
           keyExtractor={(item) => item.chatRoomId.toString()}
           // 아이템들을 렌더링하는 메서드
           renderItem={({ item }) => (
-            <ChatRoomComponent chatRoomData={item} navigation={navigation} />
+            <ChatRoomComponent
+              chatRoomData={item}
+              navigation={navigation}
+              handleExitModal={handleExitModal}
+            />
           )}
           // FlatList의 최상단에 렌더링되는 Header 아이템
           ListHeaderComponent={<HeaderComponent navigation={navigation} />}
@@ -35,6 +58,16 @@ const ChatBoxScreen = ({ navigation }: ChatBoxScreenProps) => {
           ItemSeparatorComponent={() => <View className="h-3" />}
         />
       </View>
+
+      <CustomTwoBtnModal
+        isVisible={isExitModalVisible}
+        closeModal={() => setIsExitModalVisible(false)}
+        title="쪽지함에서 나가시겠습니까?"
+        leftBtnText="취소"
+        leftBtnFunc={() => setIsExitModalVisible(false)}
+        rightBtnText="나가기"
+        rightBtnFunc={handleExit}
+      />
     </SafeArea>
   );
 };
